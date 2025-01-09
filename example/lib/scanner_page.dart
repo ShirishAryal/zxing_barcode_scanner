1b705b358f54b22927e9765c6fef48c650ab5540
Shirish Aryal <aryalshirish4@gmail.com>
2025-01-09 15:21:43 +0545

    update example

--- example/lib/scanner_page.dart
+++ example/lib/scanner_page.dart
@@ -1,10 +1,29 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:zxing_barcode_scanner/zxing_barcode_scanner.dart';
 
-class ScannerPage extends StatelessWidget {
+class ScannerPage extends StatefulWidget {
   const ScannerPage({super.key});
+
+  @override
+  State<ScannerPage> createState() => _ScannerPageState();
+}
+
+class _ScannerPageState extends State<ScannerPage> {
+  late final ZxingBarcodeScannerController _controller;
+  bool _isProcessingQR = false;
+
+  @override
+  void initState() {
+    _controller = ZxingBarcodeScannerController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +31,48 @@ class ScannerPage extends StatelessWidget {
       appBar: AppBar(),
       body: Center(
         child: ZxingBarcodeScanner(
-          onScan: (results) {
-            for (final result in results) {
-              log('Barcode Data: ${result.format ?? ''}');
-              log('Barcode Type: ${result.text ?? ''}');
-            }
+          onScan: (results) async {
+            if (_isProcessingQR) return;
+            _isProcessingQR = true;
+            await _controller.stop();
+            if (!context.mounted) return;
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ScannerResultPage(results: results),
+              ),
+            );
+            _isProcessingQR = false;
+            await _controller.start();
           },
+        ),
+      ),
+    );
+  }
+}
+
+class ScannerResultPage extends StatelessWidget {
+  const ScannerResultPage({super.key, required this.results});
+  final List<BarcodeResult> results;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (final result in results)
+                ListTile(
+                  leading: Text(
+                    result.format.toString(),
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  title: Text(result.text ?? ''),
+                ),
+            ],
+          ),
         ),
       ),
     );
