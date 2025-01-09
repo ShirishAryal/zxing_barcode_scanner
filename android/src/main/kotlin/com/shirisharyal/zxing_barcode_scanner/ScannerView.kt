f685a8f77d321ad73e937e0be88a734c91e37b2c
Shirish Aryal <aryalshirish4@gmail.com>
2025-01-09 14:11:50 +0545

    remove unused Toast import in ScannerView

--- android/src/main/kotlin/com/shirisharyal/zxing_barcode_scanner/ScannerView.kt
+++ android/src/main/kotlin/com/shirisharyal/zxing_barcode_scanner/ScannerView.kt
@@ -12,7 +12,6 @@ import android.view.Gravity
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
