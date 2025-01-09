ee5f93edbde6bae836e6c19da0f234a35d89c866
Shirish Aryal <aryalshirish4@gmail.com>
2025-01-10 00:31:18 +0545

    implement ZxingBarcodeScanner widget with platform-specific views

---
+++ lib/src/zxing_barcode_scanner.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:zxing_barcode_scanner/generated/zxing_barcode_scanner_api.g.dart';
+
+const _viewType = 'zxing_barcode_scanner';
+
+class ZxingBarcodeScanner extends StatefulWidget {
+  const ZxingBarcodeScanner({super.key, required this.onScan});
+
+  final void Function(List<BarcodeResult> results) onScan;
+
+  @override
+  State<ZxingBarcodeScanner> createState() => _ZxingBarcodeScannerState();
+}
+
+class _ZxingBarcodeScannerState extends State<ZxingBarcodeScanner> implements ZxingBarcodeScannerFlutterApi {
+  // TODO: Implement the barcode scanner creation parameters
+  final _creationParams = <String, dynamic>{};
+
+  @override
+  Widget build(BuildContext context) {
+    return switch (defaultTargetPlatform) {
+      TargetPlatform.android => AndroidView(
+          viewType: _viewType,
+          layoutDirection: TextDirection.ltr,
+          creationParams: _creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+        ),
+      TargetPlatform.iOS => UiKitView(
+          viewType: _viewType,
+          layoutDirection: TextDirection.ltr,
+          creationParams: _creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+        ),
+      _ => throw UnimplementedError(),
+    };
+  }
+
+  @override
+  void onScanSuccess(List<BarcodeResult> results) => widget.onScan(results);
+}
--- lib/zxing_barcode_scanner.dart
+++ lib/zxing_barcode_scanner.dart
@@ -1,1 +1,1 @@
-
+export './generated/zxing_barcode_scanner_api.g.dart';
